9167be6d3c026c7becb7789c6d52a585edace664
Trifa20731 <54383330+Trifa20731@users.noreply.github.com>
2021-05-03 14:36:08 +0800

    New Update 5/3/2021
    The mechanism on the adding event and case page is finished.

--- demo_config/covid/views.py
+++ demo_config/covid/views.py
@@ -10,7 +10,7 @@ def test(request):
     return HttpResponse("Hello world")
 
 def CaseFormView(request):
-    eventFormSet = modelformset_factory(Event, form = EventForm, extra=1)
+    eventFormSet = modelformset_factory(Event, form = newEventForm, extra=1)
 
     if request.method == 'POST':
         form = CaseForm(request.POST)
@@ -27,14 +27,31 @@ def CaseFormView(request):
             deltaCO = confirm_date - onset_date
 
             if (deltaBO > timedelta(days=0) and deltaCO > timedelta(days=0))  :
-                messages.success(request, 'You have add the new case successfully!') 
+                for attended_event in form.cleaned_data['events']:
+                    end_date = attended_event.date + timedelta(days=14)
+                    
+                    if onset_date > end_date or attended_event.date >confirm_date:
+                        messages.error(request, "case date out of range")
+                        return render(request, 'case_form.html', {'form': form, 'formset': formset})
+
+
+                for new_event_form in formset:
+                    if len(new_event_form.cleaned_data) == 0:
+                        break
+                    event_date = new_event_form.cleaned_data['date']
+                    # start_date = event_date - timedelta(days=3)
+                    end_date = event_date + timedelta(days=14)
+
+                    if onset_date > end_date or event_date > confirm_date:
+                        messages.error(request, "case date out of range")
+                        return render(request, 'case_form.html', {'form': form, 'formset': formset})
+
+                messages.success(request, 'You have add the new case successfully!')         
                 new_case = form.save()
                 new_events = formset.save()
                 form = CaseForm()
-
                 for event in new_events:
                     new_case.events.add(event)
-                # return redirect('test')
             else:
                 messages.error(request, 'Wrong date input.' + str(form.cleaned_data['birth']) )
         else:
@@ -57,6 +74,12 @@ def EventFormView(request):
             if startDateDelta < timedelta(days=0) or endDateDelta < timedelta(days=0):
                 messages.error(request, str(form.cleaned_data['date']) + " is out off the covid period in Hong Kong." )
             else:
+                cases = form.cleaned_data['cases']
+                event_date = form.cleaned_data['date']
+                end_date = event_date + timedelta(days=14)
+                for case in cases:
+                    if case.onset_date > end_date or case.confirm_date < event_date:
+                        messages.error(request, "case date out of range")
                 new_event = form.save()
                 messages.success(request, 'You have add the new event successfully!') 
                 form = EventForm()
@@ -70,6 +93,7 @@ def EventFormView(request):
     return render(request, 'event_form.html', {'form': form})
 
 def AddAttendanceView(request, add_type, id_num):
+
 
     if add_type == 'case':
         data_obj = Case.objects.get(pk = id_num)
@@ -89,7 +113,19 @@ def AddAttendanceView(request, add_type, id_num):
     else:
         if add_type == 'case':
             form = EventToCaseForm(instance=data_obj)
+            
+            confirm_date = data_obj.confirm_date
+            infect_date = data_obj.onset_date - timedelta(days=14)
+            valid_events = Event.objects.filter(date__lte=confirm_date)
+            valid_events = valid_events.filter(date__gte=infect_date)
+            form.fields['events'] = ModelMultipleChoiceField(queryset=valid_events,widget=CheckboxSelectMultiple)
         elif add_type == 'event':
             form = CaseToEventForm(instance=data_obj) 
+            
+            event_date = data_obj.date
+            end_date = event_date + timedelta(days=14)
+            valid_cases = Case.objects.filter(confirm_date__gt=event_date)
+            valid_cases = valid_cases.filter(onset_date__lte=end_date)
+            form.fields['cases'] = ModelMultipleChoiceField(queryset=valid_cases,widget=CheckboxSelectMultiple)
 
     return render(request, 'add_attendance_form.html', {'form': form, 'add_type': add_type, 'attendance': data_obj})
